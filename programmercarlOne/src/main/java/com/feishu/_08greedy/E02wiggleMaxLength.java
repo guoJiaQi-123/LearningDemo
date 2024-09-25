98be9d902929e748c2ad0f7e60907ff21e4d526e
GuoJiaqi <2667629684@qq.com>
2024-09-26 00:07:40 +0800

    每日算法2024年9月25日

--- programmercarlOne/src/main/java/com/feishu/_08greedy/E02wiggleMaxLength.java
+++ programmercarlOne/src/main/java/com/feishu/_08greedy/E02wiggleMaxLength.java
@@ -7,6 +7,24 @@ package com.feishu._08greedy;
  */
 public class E02wiggleMaxLength {
     public int wiggleMaxLength(int[] nums) {
-
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        //当前差值
+        int curDiff = 0;
+        //上一个差值
+        int preDiff = 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            //得到当前差值
+            curDiff = nums[i] - nums[i - 1];
+            //如果当前差值和上一个差值为一正一负
+            //等于0的情况表示初始时的preDiff
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                count++;
+                preDiff = curDiff;
+            }
+        }
+        return count;
     }
 }
