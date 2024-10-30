fb0bc09d250ff00a50d3421b3d2d7c5256126eeb
GuoJiaqi <2667629684@qq.com>
2024-10-30 19:15:18 +0800

    每日算法2024/10/28

---
+++ programmercarlOne/src/main/java/com/feishu/_01array/E15search.java
@@ -0,0 +1,40 @@
+package com.feishu._01array;
+
+/**
+ * @version v1.0
+ * @author OldGj 2024/10/27
+ * @apiNote 704. 二分查找
+ */
+public class E15search {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
