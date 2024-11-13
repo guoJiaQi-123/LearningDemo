94b7d50424dda9bf09cbe63995b3aec1b034feb8
GuoJiaqi <guojiaqi5946@outlook.com>
2024-11-13 23:42:39 +0800

    「修改」343. 整数拆分

--- programmercarlOne/src/main/java/com/feishu/_09DynamicProgramming/E06integerBreak.java
+++ programmercarlOne/src/main/java/com/feishu/_09DynamicProgramming/E06integerBreak.java
@@ -13,10 +13,10 @@ public class E06integerBreak {
         for (int i = 3; i <= n; i++) {
             for (int j = 1; j <= i >>> 1; j++) {
                 //并且，在本题中，我们分析 dp[0], dp[1]都是无意义的，
-                //j 最大到 i-j,就不会用到 dp[0]与dp[1]
+                // j 最大到 i-j,就不会用到 dp[0]与dp[1]
                 dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
                 // j * (i - j) 是单纯的把整数 i 拆分为两个数 也就是 j,i-j ，再相乘
-                //而j * dp[i - j]是将 i 拆分成两个以及两个以上的个数,再相乘。
+                // 而j * dp[i - j]是将 i 拆分成两个以及两个以上的个数,再相乘。
             }
         }
         return dp[n];
