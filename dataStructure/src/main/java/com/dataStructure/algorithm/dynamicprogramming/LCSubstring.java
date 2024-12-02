df23725fca819a2563d1e4d5fb12d3dbcfcfe900
GuoJiaqi <guojiaqi5946@outlook.com>
2024-12-02 23:19:51 +0800

    「更新」最长公共子串问题注释优化

--- dataStructure/src/main/java/com/dataStructure/algorithm/dynamicprogramming/LCSubstring.java
+++ dataStructure/src/main/java/com/dataStructure/algorithm/dynamicprogramming/LCSubstring.java
@@ -19,18 +19,30 @@ public class LCSubstring {
          m  0   0   0   0   0   1   0
          a  0   0   0   0   0   0   2
 
+         if(遇到相同字符){
+            dp[i][j] = dp[i-1][j-1]
+         }else{
+            dp[i][j] = 0
+         }
+
      */
 
+    /**
+     * 最长公共子串 - 动态规划
+     * @param a
+     * @param b
+     * @return
+     */
     private static int lcs(String a, String b) {
         int[][] dp = new int[a.length()][b.length()];
         int max = 0;
         for (int i = 0; i < a.length(); i++) {
             for (int j = 0; j < b.length(); j++) {
-                if (a.charAt(i) == b.charAt(j)) {
+                if (a.charAt(i) == b.charAt(j)) { // 遇到相同字符
                     if (i == 0 || j == 0) {
                         dp[i][j] = 1;
                     } else {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                        dp[i][j] = dp[i - 1][j - 1] + 1; // 对角线 > 上一行上一列的值 + 1
                     }
                     max = Integer.max(dp[i][j], max);
                 }
